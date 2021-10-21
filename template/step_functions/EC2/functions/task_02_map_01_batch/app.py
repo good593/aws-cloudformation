6bd4165b3ed9f31daaad453d8a73013349d9c8c1
good593 <good593@kyowon.co.kr>
2021-10-21 14:31:27 +0900

    Catch 적용

--- template/step_functions/EC2/functions/task_02_map_01_batch/app.py
+++ template/step_functions/EC2/functions/task_02_map_01_batch/app.py
@@ -6,7 +6,7 @@ import time
 from botocore.config import Config
 
 
-def lambda_handler(event, content):
+def lambda_handler(event):
   age = event['age'] * 10
 
   return {
@@ -15,24 +15,23 @@ def lambda_handler(event, content):
   }
 
 
-if __name__ == '__main__':
-  # https://gonigoni.kr/posts/step-function-with-python/
-  # https://stackoverflow.com/questions/55343073/is-there-a-way-to-retrieve-the-step-function-tasktoken-from-within-a-batch-job
-  client = boto3.client('stepfunctions', config=Config(read_timeout=70))
-  task_token = os.environ['TASK_TOKEN']
+# https://gonigoni.kr/posts/step-function-with-python/
+# https://stackoverflow.com/questions/55343073/is-there-a-way-to-retrieve-the-step-function-tasktoken-from-within-a-batch-job
+client = boto3.client('stepfunctions', config=Config(read_timeout=70))
+task_token = os.environ['TASK_TOKEN']
 
-  try:
+try:
 
-    client.send_task_heartbeat(taskToken=task_token)
+  client.send_task_heartbeat(taskToken=task_token)
 
-    params = {
-      'stage_type': os.environ['stage_type'],
-      'name': os.environ['name'],
-      'age': os.environ['age']
-    }
+  params = {
+    'stage_type': os.environ['stage_type'],
+    'name': os.environ['name'],
+    'age': os.environ['age']
+  }
 
-    result = lambda_handler(params)
-    client.send_task_success(taskToken=task_token, output=json.dumps(result))
+  result = lambda_handler(params)
+  client.send_task_success(taskToken=task_token, output=json.dumps(result))
 
-  except Exception as e:
-    client.send_task_failure(taskToken=task_token)
+except Exception as e:
+  client.send_task_failure(taskToken=task_token)
