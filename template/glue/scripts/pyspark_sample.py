f6060fb5dd1613b8c32207b16fa4d4d491a5ee84
good593 <goodwon593@gmail.com>
2022-05-24 15:43:57 +0900

    get_df_from_dynamodb

--- template/glue/scripts/pyspark_sample.py
+++ template/glue/scripts/pyspark_sample.py
@@ -10,6 +10,33 @@ from awsglue.job import Job
 from layer.common.constants import *
 from libs.script.common import *
 
+
+def get_df_from_dynamodb(pGlueContext:GlueContext, pTableNm:str, pStage:str):
+  
+  dynamic_frame = pGlueContext.create_dynamic_frame.from_options(
+    connection_type="dynamodb",
+    connection_options={
+        "dynamodb.input.tableName": "table_nm",
+        "dynamodb.throughput.read.percent": "0.1",
+        "dynamodb.splits": "1"
+    }
+  )
+  df_dynamodb = dynamic_frame.toDF()
+
+  df_dynamodb.cache()
+  df_dynamodb.createOrReplaceTempView(pTableNm)
+  return df_dynamodb
+
+def save_df_to_s3(p_df, p_mode='overwrite'):
+  # save
+  bucket = "bucket_nm"
+  batch_daily = "path"
+  save_path = bucket + batch_daily
+  
+  p_df.write.mode(p_mode).format("parquet").save(save_path)
+  # df_save.write.mode("append").format("parquet").save(save_path)
+
+
 # params
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'p_product_code', 'p_target_dt', 'p_env'])
 p_product_code = args['p_product_code']
